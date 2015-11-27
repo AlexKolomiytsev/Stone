08061dca58007b1df4a9f8a9afe387430bb7a85b
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2015-11-27 20:10:09 +0200

    Adaptive 768 .. 991(Home, About, Services, Contact)

--- content/js/nav.js
+++ content/js/nav.js
@@ -1,10 +1,13 @@
 var faUpDown = document.getElementsByClassName("arrow");
+var faLeftRight = document.getElementsByClassName("arrow768");
 
 function displaySubmenuHome() {
 	if(document.getElementsByClassName("onHoverAbout")[0].classList.contains("onHoverAboutOpen")) {
 		document.getElementsByClassName("onHoverAbout")[0].classList.remove("onHoverAboutOpen");
 		faUpDown[1].classList.remove('fa-angle-up');
 		faUpDown[1].classList.add('fa-angle-down');
+		faLeftRight[1].classList.remove('fa-angle-right');
+		faLeftRight[1].classList.add('fa-angle-left');
 	}
 	var submElem = document.getElementsByClassName("onHoverHome")[0];
 	submElem.classList.toggle('onHoverHomeOpen');
@@ -19,12 +22,22 @@ function displaySubmenuHome() {
 		faUpDown[0].classList.remove('fa-angle-up');
 		faUpDown[0].classList.add('fa-angle-down');	
 	}
+	if(faLeftRight[0].classList.contains('fa-angle-left')) {
+		faLeftRight[0].classList.remove('fa-angle-left');
+		faLeftRight[0].classList.add('fa-angle-right');
+	}
+	else {
+		faLeftRight[0].classList.remove('fa-angle-right');
+		faLeftRight[0].classList.add('fa-angle-left');
+	}
 }
 function displaySubmenuAbout() {
 	if(document.getElementsByClassName("onHoverHome")[0].classList.contains("onHoverHomeOpen")) {
 		document.getElementsByClassName("onHoverHome")[0].classList.remove("onHoverHomeOpen");
 		faUpDown[0].classList.remove('fa-angle-up');
 		faUpDown[0].classList.add('fa-angle-down');
+		faLeftRight[0].classList.remove('fa-angle-right');
+		faLeftRight[0].classList.add('fa-angle-left');
 	}
 	var submElem = document.getElementsByClassName("onHoverAbout")[0];
 	submElem.classList.toggle('onHoverAboutOpen');
@@ -38,6 +51,27 @@ function displaySubmenuAbout() {
 	else {
 		faUpDown[1].classList.remove('fa-angle-up');
 		faUpDown[1].classList.add('fa-angle-down');	
+	}
+	if(faLeftRight[1].classList.contains('fa-angle-left')) {
+		faLeftRight[1].classList.remove('fa-angle-left');
+		faLeftRight[1].classList.add('fa-angle-right');
+	}
+	else {
+		faLeftRight[1].classList.remove('fa-angle-right');
+		faLeftRight[1].classList.add('fa-angle-left');
+	}
+}
+function displayNav() {
+	var navElem = document.getElementsByClassName('nav')[0];
+	navElem.classList.toggle('navOpen');
+	var trigger = document.getElementsByClassName('trigger')[0];
+	if (trigger.classList.contains('fa-bars')) {
+		trigger.classList.remove('fa-bars');
+		trigger.classList.add('fa-times');
+	}
+	else {
+		trigger.classList.remove('fa-times');
+		trigger.classList.add('fa-bars');
 	}
 }
 
