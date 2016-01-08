ec5839e7fb23cd962b8626c7de3f83c0f54d1624
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2016-01-08 19:37:02 +0200

    Added a sticky navigation

--- content/js/nav.js
+++ content/js/nav.js
@@ -1,79 +1,106 @@
-var faUpDown = document.getElementsByClassName("arrow");
-var faLeftRight = document.getElementsByClassName("arrow768");
+window.addEventListener("load", function() {
+	var faUpDown = document.getElementsByClassName("arrow");
+	var faLeftRight = document.getElementsByClassName("arrow768");
 
-function displaySubmenuHome() {
-	if(document.getElementsByClassName("onHoverAbout")[0].classList.contains("onHoverAboutOpen")) {
-		document.getElementsByClassName("onHoverAbout")[0].classList.remove("onHoverAboutOpen");
-		faUpDown[1].classList.remove('fa-angle-up');
-		faUpDown[1].classList.add('fa-angle-down');
-		faLeftRight[1].classList.remove('fa-angle-right');
-		faLeftRight[1].classList.add('fa-angle-left');
-	}
-	var submElem = document.getElementsByClassName("onHoverHome")[0];
-	submElem.classList.toggle('onHoverHomeOpen');
-	//console.log(submElem.classList);
 
-	//console.log(faUpDown.classList
-	if(faUpDown[0].classList.contains('fa-angle-down')) {
-		faUpDown[0].classList.remove('fa-angle-down');
-		faUpDown[0].classList.add('fa-angle-up');	
-	}
-	else {
-		faUpDown[0].classList.remove('fa-angle-up');
-		faUpDown[0].classList.add('fa-angle-down');	
-	}
-	if(faLeftRight[0].classList.contains('fa-angle-left')) {
-		faLeftRight[0].classList.remove('fa-angle-left');
-		faLeftRight[0].classList.add('fa-angle-right');
-	}
-	else {
-		faLeftRight[0].classList.remove('fa-angle-right');
-		faLeftRight[0].classList.add('fa-angle-left');
-	}
-}
-function displaySubmenuAbout() {
-	if(document.getElementsByClassName("onHoverHome")[0].classList.contains("onHoverHomeOpen")) {
-		document.getElementsByClassName("onHoverHome")[0].classList.remove("onHoverHomeOpen");
-		faUpDown[0].classList.remove('fa-angle-up');
-		faUpDown[0].classList.add('fa-angle-down');
-		faLeftRight[0].classList.remove('fa-angle-right');
-		faLeftRight[0].classList.add('fa-angle-left');
-	}
-	var submElem = document.getElementsByClassName("onHoverAbout")[0];
-	submElem.classList.toggle('onHoverAboutOpen');
+	var submenu768Home = document.getElementById('submenu768Home');
+	var submenuHome = document.getElementById('submenuHome');
+	submenu768Home.addEventListener("click", displaySubmenuHome);
+	submenuHome.addEventListener("click", displaySubmenuHome);
 
-	//var faUpDown = document.getElementsByClassName("arrow");
-	//console.log(faUpDown.classList
-	if(faUpDown[1].classList.contains('fa-angle-down')) {
-		faUpDown[1].classList.remove('fa-angle-down');
-		faUpDown[1].classList.add('fa-angle-up');	
+
+	function displaySubmenuHome() {
+		if(document.getElementsByClassName("onHoverAbout")[0].classList.contains("onHoverAboutOpen")) {
+			document.getElementsByClassName("onHoverAbout")[0].classList.remove("onHoverAboutOpen");
+			faUpDown[1].classList.remove('fa-angle-up');
+			faUpDown[1].classList.add('fa-angle-down');
+			faLeftRight[1].classList.remove('fa-angle-right');
+			faLeftRight[1].classList.add('fa-angle-left');
+		}
+		var submElem = document.getElementsByClassName("onHoverHome")[0];
+		submElem.classList.toggle('onHoverHomeOpen');
+
+		if(faUpDown[0].classList.contains('fa-angle-down')) {
+			faUpDown[0].classList.remove('fa-angle-down');
+			faUpDown[0].classList.add('fa-angle-up');
+		}
+		else {
+			faUpDown[0].classList.remove('fa-angle-up');
+			faUpDown[0].classList.add('fa-angle-down');
+		}
+		if(faLeftRight[0].classList.contains('fa-angle-left')) {
+			faLeftRight[0].classList.remove('fa-angle-left');
+			faLeftRight[0].classList.add('fa-angle-right');
+		}
+		else {
+			faLeftRight[0].classList.remove('fa-angle-right');
+			faLeftRight[0].classList.add('fa-angle-left');
+		}
 	}
-	else {
-		faUpDown[1].classList.remove('fa-angle-up');
-		faUpDown[1].classList.add('fa-angle-down');	
+
+	var submenu768About = document.getElementById('submenu768About');
+	var submenuAbout = document.getElementById('submenuAbout');
+	submenu768About.addEventListener("click", displaySubmenuAbout);
+	submenuAbout.addEventListener("click", displaySubmenuAbout);
+
+
+	function displaySubmenuAbout() {
+		if(document.getElementsByClassName("onHoverHome")[0].classList.contains("onHoverHomeOpen")) {
+			document.getElementsByClassName("onHoverHome")[0].classList.remove("onHoverHomeOpen");
+			faUpDown[0].classList.remove('fa-angle-up');
+			faUpDown[0].classList.add('fa-angle-down');
+			faLeftRight[0].classList.remove('fa-angle-right');
+			faLeftRight[0].classList.add('fa-angle-left');
+		}
+		var submElem = document.getElementsByClassName("onHoverAbout")[0];
+		submElem.classList.toggle('onHoverAboutOpen');
+
+		if(faUpDown[1].classList.contains('fa-angle-down')) {
+			faUpDown[1].classList.remove('fa-angle-down');
+			faUpDown[1].classList.add('fa-angle-up');
+		}
+		else {
+			faUpDown[1].classList.remove('fa-angle-up');
+			faUpDown[1].classList.add('fa-angle-down');
+		}
+		if(faLeftRight[1].classList.contains('fa-angle-left')) {
+			faLeftRight[1].classList.remove('fa-angle-left');
+			faLeftRight[1].classList.add('fa-angle-right');
+		}
+		else {
+			faLeftRight[1].classList.remove('fa-angle-right');
+			faLeftRight[1].classList.add('fa-angle-left');
+		}
 	}
-	if(faLeftRight[1].classList.contains('fa-angle-left')) {
-		faLeftRight[1].classList.remove('fa-angle-left');
-		faLeftRight[1].classList.add('fa-angle-right');
-	}
-	else {
-		faLeftRight[1].classList.remove('fa-angle-right');
-		faLeftRight[1].classList.add('fa-angle-left');
-	}
-}
-function displayNav() {
-	var navElem = document.getElementsByClassName('nav')[0];
-	navElem.classList.toggle('navOpen');
+
 	var trigger = document.getElementsByClassName('trigger')[0];
-	if (trigger.classList.contains('fa-bars')) {
-		trigger.classList.remove('fa-bars');
-		trigger.classList.add('fa-times');
-	}
-	else {
-		trigger.classList.remove('fa-times');
-		trigger.classList.add('fa-bars');
-	}
-}
+	trigger.addEventListener("click", displayNav);
 
-// TypeError: document.getElementById(...) is null
-// var submElemIDhome = document.getElementById("submenuHome").addEventListener("click", displaySubmenuHome);
+	function displayNav() {
+		var navElem = document.getElementsByClassName('nav')[0];
+		navElem.classList.toggle('navOpen');
+		var trigger = document.getElementsByClassName('trigger')[0];
+		if (trigger.classList.contains('fa-bars')) {
+			trigger.classList.remove('fa-bars');
+			trigger.classList.add('fa-times');
+		}
+		else {
+			trigger.classList.remove('fa-times');
+			trigger.classList.add('fa-bars');
+		}
+	}
+
+	var nav = document.getElementsByClassName('navcol')[0];
+	var stickyHeader = document.getElementsByClassName('static-nav')[0];
+	var sourceNavPosition = nav.getBoundingClientRect().bottom + window.pageYOffset;
+	function isStickyNavigation() {
+		if (window.pageYOffset > sourceNavPosition) {
+			stickyHeader.classList.add('isStickyNav');
+		}
+		else {
+			stickyHeader.classList.remove('isStickyNav');
+		}
+	}
+	window.addEventListener("scroll", isStickyNavigation);
+});
+
